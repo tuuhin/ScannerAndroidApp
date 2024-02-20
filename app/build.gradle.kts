5700feba78bfdaa31aff86e0dd28407830e3a6ac
tuuhin <tuhinbhowmick2513@gmail.com>
2024-02-20 20:43:51 +0530

    Added hilt,camera and ml dependencies. Hilt added and configured for the app. Camera dependency and ml dependencies for barcode scanning and image labeling are added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
 	id("com.android.application")
 	kotlin("android")
+	id("com.google.devtools.ksp")
+	id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -56,11 +58,21 @@ dependencies {
 	implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
 	implementation("androidx.activity:activity-compose:1.8.2")
 	//compose
-	implementation(platform("androidx.compose:compose-bom:2024.02.0"))
+	implementation(platform("androidx.compose:compose-bom:2024.02.00"))
 	implementation("androidx.compose.ui:ui")
 	implementation("androidx.compose.ui:ui-graphics")
 	implementation("androidx.compose.ui:ui-tooling-preview")
 	implementation("androidx.compose.material3:material3")
+	//dagger hilt
+	implementation("com.google.dagger:hilt-android:2.50")
+	ksp("com.google.dagger:hilt-android-compiler:2.50")
+	//on device ml
+	implementation("com.google.android.gms:play-services-mlkit-barcode-scanning:18.3.0")
+	implementation("com.google.android.gms:play-services-mlkit-image-labeling:16.0.8")
+	//camerax
+	implementation("androidx.camera:camera-camera2:1.3.1")
+	implementation("androidx.camera:camera-lifecycle:1.3.1")
+	implementation("androidx.camera:camera-mlkit-vision:1.4.0-alpha04")
 	//test
 	testImplementation("junit:junit:4.13.2")
 	// test-android
@@ -68,7 +80,7 @@ dependencies {
 	androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 	androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
 	androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-	// debug
+	//debug
 	debugImplementation("androidx.compose.ui:ui-tooling")
 	debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
